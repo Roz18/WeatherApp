8f2a4f211774d4e2c0bbdeb1f64d8533df44d441
Roz18 <114506732+Roz18@users.noreply.github.com>
2023-05-29 15:40:53 +0200

    DailyW
    working on weather chart and icon

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ $(document).ready(function () {
       .get(url)
       .then(function (response) {
         let temperature = Math.round(response.data.main.temp);
+        console.log("Temperature:", temperature);
+
         $("#curentTemp").text(`${temperature}°C`);
+        sevenDayForecast(city);
       })
       .catch(function (error) {
         console.log("Error:", error);
@@ -42,6 +45,9 @@ $(document).ready(function () {
         .then((response) => response.json())
         .then((data) => {
           searchCity(data);
+          let temperature = Math.round(data.main.temp);
+          console.log("Temperature:", temperature);
+          $("#curentTemp").text(`${temperature}°C`);
         })
         .catch((error) => {
           console.log("Error:", error);
@@ -83,3 +89,61 @@ $(document).ready(function () {
 
   setInterval(updateDateTime, 1000);
 });
+
+function sevenDayForecast(city) {
+  let cnt = 7;
+  let key = "32ecda5bta3bd6bc964176affb080o6a";
+  let url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${cnt}&appid=${key}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherData = data.list.map((day) => {
+        return {
+          day: getWeekday(day.dt),
+          temp: `${Math.round(day.temp.max)}°C/${Math.round(day.temp.min)}°C`,
+          emoji: getEmoji(day.weather[0].id),
+        };
+      });
+
+      updateWeatherForecast(weatherData);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+  console.log(sevenDayForecast);
+}
+
+function updateWeatherForecast(weatherData) {
+  const forecastElements = document.querySelectorAll("#weatherForecast .day");
+  const tempElements = document.querySelectorAll("#weatherForecast .temp");
+  const emojiElements = document.querySelectorAll("#weatherForecast .emoji");
+
+  for (let i = 0; i < weatherData.length; i++) {
+    const weather = weatherData[i];
+    const forecastElement = forecastElements[i];
+    const tempElement = tempElements[i];
+    const emojiElement = emojiElements[i];
+
+    forecastElement.textContent = weather.day;
+    tempElement.textContent = weather.temp;
+    emojiElement.textContent = weather.emoji;
+  }
+}
+
+function getWeekday(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekday = days[date.getDay()];
+  return weekday;
+}
+
+function getEmoji(weatherCode) {}
